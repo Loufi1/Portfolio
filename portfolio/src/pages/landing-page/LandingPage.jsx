1916698b6d5c729ad0f8233330dd8f1cc5d705b8
Louis-Frédéric Fortier <louis-frederic@concilio.com>
2020-06-05 18:55:43 -0400

    [+](Projects) Add projects page

--- portfolio/src/pages/landing-page/LandingPage.jsx
+++ portfolio/src/pages/landing-page/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Col, Row, Container} from "react-bootstrap";
-import Carousel from "react-bootstrap/Carousel";
+import Card from "react-bootstrap/Card";
 
 const FADE_RATIO = 5;
 
@@ -75,7 +75,7 @@ export default class LandingPage extends React.Component {
                                 <Col className="text-center mt-5" xs={6}>
                                     <p>
                                         Hello, I am <br/>
-                                        <span>Louis-Frédéric</span> <br/>
+                                        <span className='name'>Louis-Frédéric</span> <br/>
                                         a web and mobile fullstack developer
                                     </p>
                                 </Col>
@@ -97,20 +97,92 @@ export default class LandingPage extends React.Component {
                                     <h1>Projects</h1>
                                 </Col>
                             </Row>
-                            <Row className="mt-5">
-                                <Col className="text-center mt-5 ml-5 projects" xs={4}>
-                                    <img src={require('../../assets/Buzzle.png')} height={75}/>
+                            <Row className="mt-5 ml-2 row-table">
+                                <Col className="text-center mt-5 projects row-cell" xs={3}>
+                                    <Card>
+                                        <Card.Header as="h5">Buzzle</Card.Header>
+                                        <Card.Body>
+                                            <Card.Title>
+                                                <Container fluid={true}>
+                                                    <Row>
+                                                        <Col>
+                                                            <img alt={''} src={require('../../assets/Buzzle.png')} height={75}/>
+                                                        </Col>
+                                                        <Col>
+                                                            <span className='project-description'>
+                                                                Buzzle is an intelligent alarm who handle traffic disruption.
+                                                            </span>
+                                                        </Col>
+                                                    </Row>
+                                                </Container>
+                                            </Card.Title>
+                                        </Card.Body>
+                                    </Card>
                                 </Col>
-                                <Col className="text-center mt-5 ml-5 projects" xs={2}>
-                                    <img src={require('../../assets/area.png')} height={75}/>
+                                <Col className="text-center mt-5 projects row-cell" xs={3}>
+                                    <Card>
+                                        <Card.Header as="h5">Area</Card.Header>
+                                        <Card.Body>
+                                            <Card.Title>
+                                                <Container fluid={true}>
+                                                    <Row>
+                                                        <Col>
+                                                            <img alt={''} src={require('../../assets/area.png')} height={75}/>
+                                                        </Col>
+                                                        <Col>
+                                                            <span className='project-description'>
+                                                                Area is an IFTTT / Zapier like, made for an Epitech project.
+                                                            </span>
+                                                        </Col>
+                                                    </Row>
+                                                </Container>
+                                            </Card.Title>
+                                        </Card.Body>
+                                    </Card>
                                 </Col>
                             </Row>
-                            <Row className="mt-5">
-                                <Col className="text-center mt-5 ml-5 projects" xs={2}>
-                                    <img src={require('../../assets/uikit.png')} height={75}/>
+                            <Row className="mt-5 ml-2 row-table">
+                                <Col className="text-center mt-1 projects row-cell" xs={3}>
+                                    <Card>
+                                        <Card.Header as="h5">Uikit</Card.Header>
+                                        <Card.Body>
+                                            <Card.Title>
+                                                <Container fluid={true}>
+                                                    <Row>
+                                                        <Col>
+                                                            <img alt={''} src={require('../../assets/uikit.png')} height={75}/>
+                                                        </Col>
+                                                        <Col>
+                                                            <span className='project-description'>
+                                                                Uikit is a flutter sample project with many prefabs widgets.
+                                                            </span>
+                                                        </Col>
+                                                    </Row>
+                                                </Container>
+                                            </Card.Title>
+                                        </Card.Body>
+                                    </Card>
                                 </Col>
-                                <Col className="text-center mt-5 ml-5 projects" xs={2}>
-                                    <img src={require('../../assets/blockchain.png')} height={75}/>
+                                <Col className="text-center mt-1 ml-2 projects row-cell" xs={3}>
+                                    <Card>
+                                        <Card.Header as="h5">Javascript blockchain</Card.Header>
+                                        <Card.Body>
+                                            <Card.Title>
+                                                <Container fluid={true}>
+                                                    <Row>
+                                                        <Col>
+                                                            <img alt={''} src={require('../../assets/blockchain.png')} height={75}/>
+                                                        </Col>
+                                                        <Col>
+                                                            <span className='project-description'>
+                                                                Javascript blockchain is a centralized blockchain.
+                                                            </span>
+                                                        </Col>
+                                                    </Row>
+                                                </Container>
+                                            </Card.Title>
+                                        </Card.Body>
+                                    </Card>
                                 </Col>
                             </Row>
                             <img style={{ right: 40, bottom: 50, position: 'absolute'}} src={require("../../assets/project.svg")} alt={''} height={window.innerWidth / 3} />
